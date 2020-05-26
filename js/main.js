575fb7886445f13212bd21394b2196619bba448c
Suraj_Banthot <surajbanthot@gmail.com>
2020-05-26 18:53:32 +0530

    contacts Sect Imp. with the use of GCP

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // Hello javaScript
+
+// Maps extension on the contact us.
+
+function initMap()
+{
+    const loc ={ lat: 42.361145, lan: -71.057003}
+};
+
+const map = new google.maps.Map(document.querySelector('.map'),
+{
+    zoom: 14,
+    center: loc
+});
+
+const marker = new google.maps.Marker({ position: loc, map: map});
